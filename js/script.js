6b71b249aef2dec653f6d263907fdf4fb60ed369
idrisselbergui <you@example.com>
2023-11-16 16:42:14 +0100

    home

--- js/script.js
+++ js/script.js
@@ -1,22 +1,78 @@
  let items_holder=document.getElementById("items");
  let items=[
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10],
-    ["cafe","images/cafe.jpg",10]
+   {
+      nom:"tea",
+      image:"images/cafe.jpg",
+      prix:"10"
+   },
+   {
+      nom:"black_cafe",
+      image:"images/cafe.jpg",
+      prix:"10"
+      
+   },
+   {
+      nom:"Piza",
+      image:"images/cafe.jpg",
+      prix:"30"
+   },
+   {
+      nom:"cafe",
+      image:"images/cafe.jpg",
+      prix:"10"
+   },
+   {
+      nom:"Tajine",
+      image:"images/cafe.jpg",
+      prix:"50"
+   },
+   {
+      nom:"Tajine",
+      image:"images/cafe.jpg",
+      prix:"50"
+   }
     
  ]
 
+ var orders=[
+
+ ]
+ load_image();
  function load_image(){
-    for(i=0;i<items.length;i++){
-        
-    }
+   items.forEach((item)=>{
+      let image=document.createElement("img");
+      image.setAttribute("width","140px");
+      image.setAttribute("height","120px");
+      image.setAttribute("id",item.nom);
+      image.setAttribute("src",item.image);
+      let info_holder=document.createElement("div");
+      info_holder.setAttribute("onclick","place_order()")
+      let item_name=document.createElement("h3");
+      item_name.textContent=item.nom+"  "+item.prix+" dh";
+      info_holder.classList="infoholder";
+      info_holder.appendChild(image);
+      info_holder.appendChild(item_name);
+      items_holder.appendChild(info_holder);
+      items_holder.appendChild(info_holder);
+   })
+    
 
  }
+   function place_order(){
+      let my_table=document.getElementById("my_table")
+      orders.forEach((element)=>{
+         let new_order=document.createElement("tr")
+         element.forEach((info)=>{
+            let td=document.createElement("td")
+            td.textContent=info;
+            console.log(info)
+            new_order.appendChild(tr);
+         })
+         my_table.appendChild(new_order)
 
- 
+      })
+      
+      
+      
+   
+} 
