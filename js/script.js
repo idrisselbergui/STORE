ea1be994f77d7795116e51e52a250f9469b047a8
idrisselbergui <idrisselbergui@gmail.com>
2023-11-18 10:13:17 -0500

    10

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ let total=0;
 
  ]
  load_image();
- function load_image(){
+ function load_image(a){
    items.forEach((item)=>{
       let image=document.createElement("img");
       image.setAttribute("width","140px");
@@ -69,57 +69,70 @@ function place_order(identifient){
       if( item.nom===identifient.id){
          itm=item;
       }
-      
    })
-  
-
    const new_item_to_add = [itm.nom,1,itm.prix,itm.prix];
-   if(orders.length==0){
-      orders.push(new_item_to_add);
-      total+=new_item_to_add[3]/1 ;
-   }
-   else{
-      let etat=false;
-      orders.forEach((x)=>{
-         
+   let etat=false;
+   orders.forEach((x)=>{
          if(x[0]===new_item_to_add[0]){
-            x[1]=Number(x[1])+1;
-            x[3]=x[1]*x[2]
-            
-            total+=x[3]/1 ;
-            etat=true;
-         }
-         
-      })
-      if(!etat){ 
-         orders.push(new_item_to_add);
-         total+=new_item_to_add[3]/1;
+         x[1]=Number(x[1])+1;
+         x[3]=x[1]*x[2]
+         total+=x[2]/1 ;
+         etat=true;
       }
-      
-        
-      
-      
-      }
-      total_prix.innerHTML=total;
-      console.log(total)
+   })
+   if(!etat){ 
+      orders.push(new_item_to_add);
+      total+=new_item_to_add[3]/1;
+   }
+   
+   table_implientation();
+} 
+
+
+   function table_implientation(){
       let my_table=document.getElementById("my_table")
       let table_data=document.getElementsByClassName("table_child")
       my_table.innerHTML=table_header;
       orders.forEach((el)=>{
-
-      let new_row=document.createElement("tr")
-      new_row.setAttribute("class","table_child")
-      new_row.setAttribute("id",el[0]+"row")
-      el.forEach((info)=>{
-         let td=document.createElement("td")
-         td.textContent=info;
-         new_row.appendChild(td);
-      })
+         let new_row=document.createElement("tr")
+         new_row.setAttribute("class","table_child")
+         new_row.setAttribute("id",el[0]+"row")
+         el.forEach((info)=>{
+            let td=document.createElement("td")
+            td.textContent=info;
+            new_row.appendChild(td);
+        })
+        //  button to delete item
+        let td=document.createElement("td")
+        td.textContent="X";
+        td.setAttribute("onclick","delete_row(\""+el[0]+"\")")
+        new_row.appendChild(td);
+        console.log(el[0])
+        
       my_table.appendChild(new_row)
 
-   })
-   
-   
-   
-   
-} 
+      })
+      total_prix.innerHTML=total;
+   }
+   function delete_row(x){
+      console.log(x)
+      orders.forEach((order)=>{
+         if(order[0]===x){
+            if( order[1]>1){
+
+            order[1]=Number(order[1])-1;
+            order[3]=order[1]*order[2]
+            total-=order[2]/1 ;
+           
+           
+            }
+            else{
+               total-=order[2]/1 ;
+                orders.splice(orders.indexOf(order),1)
+            }
+            table_implientation();
+         }
+
+      })
+
+   }
